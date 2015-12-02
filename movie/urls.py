60c1ee30cf5b59a401c842ff625611555f32c0ed
gopi krishna <nvdkrishna7@gmail.com>
2015-12-02 10:17:38 +0530

    Home page commit

--- movie/urls.py
+++ movie/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 	url(r'^$',views.register, name = 'register'),
-
+	# url(r'^$',views.index, name='index'),
+	url(r'^home/$',views.validate),
+	url(r'validatelogin/$',views.validatelogin, name='validatelogin'),
+	url(r'validate/$',views.validate, name='validate'),
+	# url(r'validatelogin123/$',views.validatelogin123, name='validatelogin123'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,13 @@ from django.conf import settings
 from django.conf.urls import patterns,include, url
 from django.contrib import admin
 from django.conf.urls.static import static
-from movie.views import *
+# from movie.views import *
+from movie import views
 from django.views.generic import RedirectView
 
 
 urlpatterns = [
-	url(r'^register/',include('movie.urls')),
+	url(r'^movie/',include('movie.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
